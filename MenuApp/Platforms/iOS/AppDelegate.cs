579b06bab4e575644e053e72ace9bf48948ca9c6
nick <nikole.phipps12@gmail.com>
2024-07-28 18:41:21 -0700

    Add project files.

---
+++ MenuApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MenuApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
